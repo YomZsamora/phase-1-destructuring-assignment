1a415248fbcb8a411861eb134c0cf27a62d0ecc6
yomZsamora <samora.y@adzumi.co.ke>
2022-02-03 10:41:59 +0300

    Use destructuring to assign appropriate variables based on the sounds animals make.

--- index.js
+++ index.js
@@ -3,31 +3,32 @@ const farmAnimals = 'cow horse sheep pig chicken';
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+	muppetName: 'Miss Piggy',
+	color: 'pink',
+	song: 'Never Before, Never Again',
+	job: 'Cast member of The Muppet Show',
+	partner: 'Kermit'
 };
 
 const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+	nestedName: 'Kermit',
+	nestedColor: 'green',
+	album: {
+		theMuppetMovie: {
+			song1: 'Rainbow Connection',
+			song2: 'Moving Right Along',
+			song3: 'Never Before, Never Again',
+			song4: 'I Hope That Something Better Comes Along',
+		},
+	},
+	nestedJob: 'Host of The Muppet Show',
+	nestedPartner: 'Miss Piggy'
 };
 
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
